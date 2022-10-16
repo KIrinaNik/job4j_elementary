e932af30bfa8f0dec4b517dd64568346501d5c0a
KIrinaNik <kuzmina.in@inbox.ru>
2022-10-16 21:07:46 +0400

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StugentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StugentInfo {
+    public static void main(String[] args) {
+        System.out.println("Irina Kuzmina");
+        System.out.println("13.07.1988");
+    }
+}
